e9be7273d9392c9311b38a2823a2d5509ae82cd4
Wontae <wontae.gramin9@gmail.com>
2025-10-13 22:12:34 +0100

    [Setup] Scss variable + Card & Container component

--- client/src/app/pages/home/home.ts
+++ client/src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Card } from '@components/common/card/card';
+import { Container } from '@components/common/container/container';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Card, Container],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
