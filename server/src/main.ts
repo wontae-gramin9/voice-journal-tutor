3461c1dc5b11e32584343f17e93b358050937241
Wontae <wontae.gramin9@gmail.com>
2025-12-17 22:46:03 +0000

    [Fix] CORS 허용, 이후 Azure에 연결할 때를 대비해 특정 도메인만 지정

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS 허용
+  app.enableCors({
+    origin: 'http://localhost:4200', // 클라이언트 주소
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // 쿠키 등을 주고받을때 필요
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
