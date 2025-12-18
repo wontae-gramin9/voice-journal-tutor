96e7c70557101ccb4233c10a5d3ed80b689e2397
Wontae <wontae.gramin9@gmail.com>
2025-12-18 00:35:25 +0000

    [Add] Home화면에 recording list populate

--- client/src/app/pages/home/home.ts
+++ client/src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Card } from '@components/common/card/card';
 import { Container } from '@components/common/container/container';
@@ -6,6 +6,7 @@ import { AudioRecording } from '@components/organism/audio-recording/audio-recor
 import { SentimentGraph } from '@components/organism/sentiment-graph/sentiment-graph';
 import { AudioService } from '@services/audio.service';
 import { IconButton } from '@components/common/icon-button/icon-button';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +14,21 @@ import { IconButton } from '@components/common/icon-button/icon-button';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit {
   private audioService = inject(AudioService);
   private router = inject(Router);
   recordedAudioUrl = this.audioService.audioFile;
+  audioIDs!: string[];
 
-  audioIDs = ['rec1', 'rec2', 'rec3', 'rec4'];
+  ngOnInit() {
+    this.audioService
+      .getRecordingMetadataList(new Date(0).toISOString())
+      .pipe(take(1))
+      .subscribe(recordings => {
+        this.audioIDs = recordings.map(recording => recording.uuid);
+      });
+  }
+
   trackByRecordingId(_index: number, id: string) {
     return id;
   }
